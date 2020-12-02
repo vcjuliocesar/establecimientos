b53493d05d2ab6c958c16203215017f153704b82
vcjuliocesar <vcjuliocesar@outlook.com>
2020-12-02 16:04:05 -0600

    Creando el Modelo,Controller y Migration

---
+++ app/Http/Controllers/EstableciomientoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Estableciomiento;
+use Illuminate\Http\Request;
+
+class EstableciomientoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Estableciomiento  $estableciomiento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Estableciomiento $estableciomiento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Estableciomiento  $estableciomiento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Estableciomiento $estableciomiento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Estableciomiento  $estableciomiento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Estableciomiento $estableciomiento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Estableciomiento  $estableciomiento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Estableciomiento $estableciomiento)
+    {
+        //
+    }
+}
