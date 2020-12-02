ecce2b3a774ceb621eff32b1e94892479632c2f7
vcjuliocesar <vcjuliocesar@outlook.com>
2020-12-02 17:17:02 -0600

    Creando el input de Imagen Principal y Direccion

--- resources/views/establecimientos/create.blade.php
+++ resources/views/establecimientos/create.blade.php
@@ -6,7 +6,7 @@
         <div class="mt-5 row justify-content-center">
             <form action="" class="col-md-9 col-xs-12 card card-body">
                 <fieldset class="border p-4">
-                    <legend class="text-primary">Nombre y Categoría</legend>
+                    <legend class="text-primary">Nombre, Categoría e Imagen Principal</legend>
                     <div class="form-group">
                         <label for="nombre">Nombre Establecimiento</label>
                         <input
@@ -45,6 +45,37 @@
                                     {{$message}}
                                 </div>
                             @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="imagen_principal">Imagen Principal</label>
+                        <input
+                            type="file"
+                            id="imagen_principal"
+                            class="form-control @error('imagen_principal') is-invalid @enderror"
+                            name="imagen_principal"
+                            value="{{old('imagen_principal')}}"
+                            >
+
+                            @error('imagen_principal')
+                                <div class="invalid-feedback">
+                                    {{$message}}
+                                </div>
+                            @enderror
+                    </div>
+                </fieldset>
+                <fieldset class="border p-4">
+                    <legend class="text-primary">Ubicación</legend>
+                    <div class="form-group">
+                        <label for="formbuscador">Coloca la dirección del Establecimiento</label>
+                        <input
+                            type="text"
+                            id="formbuscador"
+                            class="form-control @error('nombre') is-invalid @enderror"
+                            placeholder="Calle del Negocio o Establecimiento"
+                        >
+                        <p class="text-secondary mt-5 mb-3 text-center">
+                            El asistente colocará una dirección estimada, mueve el Pin hacia el lugar correcto
+                        </p>
                     </div>
                 </fieldset>
             </form>
