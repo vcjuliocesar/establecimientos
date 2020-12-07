59105cd400bd26d77aaf470936b017a5d42ecfca
vcjuliocesar <vcjuliocesar@outlook.com>
2020-12-07 12:35:43 -0600

    Enviando imagenes al Servidor y guardando su nombre

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Imagen;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
@@ -10,6 +12,25 @@ class ImagenController extends Controller
     public function store(Request $request)
     {
         //return $request->all();
-        return $request->file('file');
+        //leer la imagen
+        $ruta_imagen = $request->file('file')->store('establecimientos','public');
+
+        //Resize a la imagen
+        $imagen = Image::make(public_path("storage/{$ruta_imagen}"))->fit(800,450);
+        $imagen->save();
+
+        // Almacenar con modelo
+        $imagenDB = new Imagen;
+        $imagenDB->id_establecimiento = $request['uuid'];
+        $imagenDB->ruta_imagen = $ruta_imagen;
+
+        $imagenDB->save();
+
+        //Retornar respuesta
+        $respuesta = [
+            'archivo' => $ruta_imagen
+        ];
+
+        return response()->json($respuesta);
     }
 }
