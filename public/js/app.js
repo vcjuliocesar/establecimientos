f48c55a5d8baa599d7ef1b8ccb96d65c2942d6b6
vcjuliocesar <vcjuliocesar@outlook.com>
2020-12-08 12:47:17 -0600

    Consumiendo los Cafes y mostrandolos en la pagina principal

--- public/js/app.js
+++ public/js/app.js
@@ -1899,6 +1899,56 @@ module.exports = {
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/CategoriaCafe.vue?vue&type=script&lang=js&":
+/*!************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/CategoriaCafe.vue?vue&type=script&lang=js& ***!
+  \************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      cafes: []
+    };
+  },
+  mounted: function mounted() {
+    var _this = this;
+
+    axios.get('/api/categorias/cafe').then(function (respuesta) {
+      _this.cafes = respuesta.data;
+      console.log(_this.cafes);
+    });
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/PaginaInicio.vue?vue&type=script&lang=js&":
 /*!***********************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/PaginaInicio.vue?vue&type=script&lang=js& ***!
@@ -51619,7 +51669,63 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("h1", [_vm._v("Desde categoria cafe")])
+  return _c("div", { staticClass: "container my-5" }, [
+    _c("h2", [_vm._v("Cafés")]),
+    _vm._v(" "),
+    _c(
+      "div",
+      { staticClass: "row" },
+      _vm._l(this.cafes, function(cafe) {
+        return _c("div", { key: cafe.id, staticClass: "col-md-4 mt-4" }, [
+          _c("div", { staticClass: "card" }, [
+            _c("img", {
+              staticClass: "card-img-top",
+              attrs: {
+                src: "storage/" + cafe.imagen_principal,
+                alt: "card-cafe"
+              }
+            }),
+            _vm._v(" "),
+            _c("div", { staticClass: "card-body" }, [
+              _c(
+                "h3",
+                { staticClass: "card-title text-primary font-weight-bold" },
+                [
+                  _vm._v(
+                    "\n                        " +
+                      _vm._s(cafe.nombre) +
+                      "\n                    "
+                  )
+                ]
+              ),
+              _vm._v(" "),
+              _c("p", { staticClass: "card-text" }, [
+                _vm._v(_vm._s(cafe.direccion))
+              ]),
+              _vm._v(" "),
+              _c("p", { staticClass: "card-text" }, [
+                _c("span", { staticClass: "font-weight-bold" }, [
+                  _vm._v("Horario:")
+                ]),
+                _vm._v(
+                  "\n                         " +
+                    _vm._s(cafe.apertura) +
+                    " - " +
+                    _vm._s(cafe.cierre) +
+                    "\n                     "
+                )
+              ]),
+              _vm._v(" "),
+              _c("a", { staticClass: "btn btn-primary d-block" }, [
+                _vm._v("Ver lugar")
+              ])
+            ])
+          ])
+        ])
+      }),
+      0
+    )
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -63939,15 +64045,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _CategoriaCafe_vue_vue_type_template_id_5820e115___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./CategoriaCafe.vue?vue&type=template&id=5820e115& */ "./resources/js/components/CategoriaCafe.vue?vue&type=template&id=5820e115&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _CategoriaCafe_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./CategoriaCafe.vue?vue&type=script&lang=js& */ "./resources/js/components/CategoriaCafe.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _CategoriaCafe_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _CategoriaCafe_vue_vue_type_template_id_5820e115___WEBPACK_IMPORTED_MODULE_0__["render"],
   _CategoriaCafe_vue_vue_type_template_id_5820e115___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -63961,6 +64069,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "resources/js/components/CategoriaCafe.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/CategoriaCafe.vue?vue&type=script&lang=js&":
+/*!****************************************************************************!*\
+  !*** ./resources/js/components/CategoriaCafe.vue?vue&type=script&lang=js& ***!
+  \****************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_CategoriaCafe_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./CategoriaCafe.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/CategoriaCafe.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_CategoriaCafe_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
--- resources/js/components/CategoriaCafe.vue
+++ resources/js/components/CategoriaCafe.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>Desde categoria cafe</h1>
+    <div class="container my-5">
+        <h2>Cafés</h2>
+        <div class="row">
+            <div class="col-md-4 mt-4" v-for="cafe in this.cafes" v-bind:key="cafe.id">
+                <div class="card">
+                    <img class="card-img-top" :src="`storage/${cafe.imagen_principal}`" alt="card-cafe">
+                    <div class="card-body">
+                        <h3 class="card-title text-primary font-weight-bold">
+                            {{cafe.nombre}}
+                        </h3>
+                        <p class="card-text">{{cafe.direccion}}</p>
+                         <p class="card-text">
+                             <span class="font-weight-bold">Horario:</span>
+                             {{cafe.apertura}} - {{cafe.cierre}}
+                         </p>
+                         <a class="btn btn-primary d-block">Ver lugar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+export default {
+    data: function(){
+        return {
+            cafes:[]
+        }
+    },
+    mounted(){
+        axios.get('/api/categorias/cafe')
+            .then(respuesta => {
+                this.cafes=respuesta.data;
+                console.log(this.cafes);
+            })
+    }
+}
+</script>
